c929a8495f41b596ae2078109213fbd1c6302075
koekeishiya <aasvi93@hotmail.com>
2018-12-20 21:22:15 +0100

    update path

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-./bin/majic sample/s1.maji -o s1.bcr
+./bin/majic sample/s1.maji -o bin/s1.bcr
 echo ""
-../bytecode/bin/bcr -p s1.bcr
+../bytecode/bin/bcr -p bin/s1.bcr
 RET=$?
 echo "----"
 echo "return value is ${RET}"
